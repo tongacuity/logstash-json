a85c580d91fd7e83e1f5d79a927b45654ad25be8
quan tran <quan@acuity.sg>
2021-07-11 19:15:31 +0700

    ugrade connection

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule LogstashJson.Mixfile do
 
   defp deps do
     [
-      {:connection, "~> 1.0"},
+      {:connection, "~> 1.1.0"},
       {:jason, "~> 1.2", optional: true},
       {:blocking_queue, "~> 1.3", optional: true},
       {:ex_doc, ">= 0.0.0", only: :dev},
